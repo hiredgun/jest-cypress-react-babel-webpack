9fec542de99e0926058597d39deda3ab9a53bea6
Kent C. Dodds <kent@doddsfamily.us>
2018-06-25 21:14:47 -0600

    init coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,14 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
   },
-  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js')
+  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 16,
+      branches: 6,
+      functions: 20,
+      lines: 16
+    }
+  }
 }
